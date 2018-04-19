8e97057c275a80113c3b1148170ed267e121b18f
Ryan Chenkie <ryanchenkie@gmail.com>
2018-04-19 10:50:12 -0600

    setup ex 1 finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FlightsComponent } from './flights/flights.component';
-import { ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, FlightsComponent],
-  imports: [BrowserModule, RouterModule.forRoot(ROUTES), ReactiveFormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
